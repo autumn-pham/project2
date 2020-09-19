b09422bbef2ebf180c6c1c6300c5d02b8d801d31
Autumn Pham <autumnpham@Autumns-MacBook.lan>
2020-09-19 01:11:18 -0700

    fixed new user

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,7 +4,7 @@ const users = express.Router()
 const User = require('../models/users.js')
 
 users.get('/new', (req, res) => {
-  res.render('sessions/new.ejs', {
+  res.render('users/new.ejs', {
     currentUser:req.session.currentUser
   })
 })
